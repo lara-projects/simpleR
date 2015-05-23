fc88f8cb6123ac229c9c6fc0c913e020116bfa28
Tekin Aydoğdu <tekinaydogdu01@gmail.com>
2015-05-24 01:37:36 +0300

    env ve datatable düzeltmesi

--- resources/views/admin/inc/tables.blade.php
+++ resources/views/admin/inc/tables.blade.php
@@ -61,7 +61,7 @@
             <div class="panel panel-default">
                 <div class="panel-heading">Styled Table</div>
                 <div class="panel-body">
-                    <table data-toggle="table" id="table-style" data-url="tables/data2.json" data-row-style="rowStyle">
+                    <table data-toggle="table" id="table-style" data-url="{{ asset('tables/data2.json') }}" data-row-style="rowStyle">
                         <thead>
                         <tr>
                             <th data-field="id" data-align="right">Item ID</th>
